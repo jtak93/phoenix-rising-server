d6a22eebb906c8990f81dbc0fd175c0feae1d354
james tak <jtak93@gmail.com>
2017-06-27 19:23:56 -0700

    update sockets

--- sockets/base.js
+++ sockets/base.js
@@ -1,5 +1,8 @@
 module.exports = function (io) {
   io.on('connection', function(socket) {
-    console.log('connection to socket:', socket)
+    console.log('connection to socket id:', socket.conn.id)
+    socket.on('test', function(data) {
+      console.log('data:', data)
+    })
   })
 }
